861a39c9dc0a5ec75e50cb42f9d41395daca4fcb
Quadtree <quadtree@gmail.com>
2023-04-29 00:56:19 -0700

    U

--- actors/Buggy2.cs
+++ actors/Buggy2.cs
@@ -46,7 +46,7 @@ public class Buggy2 : Spatial
 
         foreach (var it in this.FindChildrenByType<Buggy2Wheel>())
         {
-            GD.Print(it.Transform.origin.x);
+            //GD.Print(it.Transform.origin.x);
             if (it.Transform.origin.x < 0)
                 it.AngularVelocity = new Vector3(leftWheelPower * -EnginePower, 0, 0);
             else
