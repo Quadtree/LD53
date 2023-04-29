a3d2dbc7be172f533a36ae2b40b9d10c1eccb47c
Quadtree <quadtree@gmail.com>
2023-04-29 13:33:42 -0700

    U

---
+++ maps/RockField.cs
@@ -0,0 +1,32 @@
+using System;
+using Godot;
+
+public class RockField : Spatial
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    void Setup()
+    {
+        for (var i = 0; i < 40; ++i)
+        {
+            var rock = GD.Load<PackedScene>($"res://actors/rocks/Rock{Util.RandInt(1, 5)}.tscn").Instance<Spatial>();
+
+            AddChild(rock);
+            rock.GlobalTranslation = GlobalTranslation + new Vector3(Util.RandF(-50, 50), 3, Util.RandF(-50, 50));
+        }
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //
+    //  }
+}
